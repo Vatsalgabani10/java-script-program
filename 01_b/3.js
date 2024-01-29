2590b09ea97247d5254efe702d281943ce02eb4c
Vatsalgabani10 <139572826+Vatsalgabani10@users.noreply.github.com>
2024-01-29 06:57:07 +0000

    datatypes and conversion

--- 01_b/3.js
+++ 01_b/3.js
@@ -1,1 +1,9 @@
-
+const accountId= 1245;
+let accountEmail= 'abc@gmail.com';
+var accountPassword='21354';
+accountCity="delhi";
+//accountId=2;
+//console.log('accountId');
+//console.table([accountId, accountEmail, accountPassword,accountCity ]);
+//console.log(typeof '');
+console.log(Number(accountPassword ));
